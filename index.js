b87e24308cc36ef1a6f290d9d9ea79a5b481550a
miShelbyT <mi.shelbyrose@gmail.com>
2023-08-17 13:41:22 -0400

    added some computer magic show and hide on user question and also form reset

--- index.js
+++ index.js
@@ -1,25 +1,49 @@
 const navLinks = document.querySelectorAll(".navlink")
-const toggleForm = document.getElementById("show-form")
+const toggleFormBtn = document.getElementById("show-form")
 const form = document.querySelector("form")
+const userQuestion = document.getElementById("user-question")
+const greeting = document.getElementById("greeting")
+let userQuestionDisplayed = false
 
-
-toggleForm.addEventListener("click", ()=> {
-  if(toggleForm.checked) form.style.display = "block"
+const toggleFunc = () => {
+  if(toggleFormBtn.checked) {
+    if(userQuestionDisplayed){
+      greeting.innerText = ""
+      userQuestion.innerText = ""
+      // p tag is empty string and ol innerText is empty string
+    }
+    form.style.display = "block"
+    document.getElementById("firstName").focus();
+  }
   else form.style.display = "none"
-})
+}
+
+toggleFormBtn.addEventListener("click", toggleFunc)
 
 form.addEventListener("submit", (e)=> {
-  const firstName = document.getElementById("firstName").value
-  const lastName = document.getElementById("lastName").value
-  const email = document.getElementById("email").value
   const emailTwo = document.getElementById("email2").value
-  const question = document.getElementById("question").value
+
+  const data = [
+    document.getElementById("firstName").value,
+    document.getElementById("lastName").value,
+    document.getElementById("email").value,
+    document.getElementById("question").value
+  ]
 
   e.preventDefault()
-  if(email !== emailTwo) alert("Email fields must match. Please correct this error.")
+  if(data[2] !== emailTwo) alert("Email fields must match. Please correct this error.")
+  else if(!data[0] || !data[1] || !data[2] || !data[3]) alert("All fields are required. Please check for empty inputs.")
   else {
-    alert(`Dear ${firstName} ${lastName}, thank you for reaching out. We will respond to your inquiry at ${email} as soon as possible.`)
+    greeting.innerText = `Dear ${data[0]} ${data[1]}, thank you for reaching out. We will respond to your inquiry as soon as possible.`
+    data.forEach(el => {
+      const entry = document.createElement("li")
+      entry.innerText = el
+      userQuestion.appendChild(entry)
+    })
     form.reset()
+    userQuestionDisplayed = !userQuestionDisplayed
+    toggleFormBtn.checked = !toggleFormBtn.checked
+    toggleFunc()
   }
 })
 
