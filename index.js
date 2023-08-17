799b3d8c07eab5467a31f751a30c9bddb9e8c253
miShelbyT <mi.shelbyrose@gmail.com>
2023-08-17 15:30:10 -0400

    media queries and updates to accomodate accessibility reqs

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ const toggleFunc = () => {
     if(userQuestionDisplayed){
       greeting.innerText = ""
       userQuestion.innerText = ""
-      // p tag is empty string and ol innerText is empty string
+      userQuestionDisplayed = !userQuestionDisplayed
+      // using a kind of state to keep track of whether there is data from a user question being displayed on the screen
     }
     form.style.display = "block"
     document.getElementById("firstName").focus();
@@ -18,34 +19,37 @@ const toggleFunc = () => {
   else form.style.display = "none"
 }
 
-toggleFormBtn.addEventListener("click", toggleFunc)
+if(toggleFormBtn && form) {
+  toggleFormBtn.addEventListener("click", toggleFunc)
 
-form.addEventListener("submit", (e)=> {
-  const emailTwo = document.getElementById("email2").value
+  form.addEventListener("submit", (e)=> {
+    const emailTwo = document.getElementById("email2").value
 
-  const data = [
-    document.getElementById("firstName").value,
-    document.getElementById("lastName").value,
-    document.getElementById("email").value,
-    document.getElementById("question").value
-  ]
+    const data = [
+      document.getElementById("firstName").value,
+      document.getElementById("lastName").value,
+      document.getElementById("email").value,
+      document.getElementById("question").value
+    ]
 
-  e.preventDefault()
-  if(data[2] !== emailTwo) alert("Email fields must match. Please correct this error.")
-  else if(!data[0] || !data[1] || !data[2] || !data[3]) alert("All fields are required. Please check for empty inputs.")
-  else {
-    greeting.innerText = `Dear ${data[0]} ${data[1]}, thank you for reaching out. We will respond to your inquiry as soon as possible.`
-    data.forEach(el => {
-      const entry = document.createElement("li")
-      entry.innerText = el
-      userQuestion.appendChild(entry)
-    })
-    form.reset()
-    userQuestionDisplayed = !userQuestionDisplayed
-    toggleFormBtn.checked = !toggleFormBtn.checked
-    toggleFunc()
-  }
-})
+    e.preventDefault()
+    if(data[2] !== emailTwo) alert("Email fields must match. Please correct this error.")
+    // else if(!data[0] || !data[1] || !data[2] || !data[3]) alert("All fields are required. Please check for empty inputs.")
+    // wasn't certain if I needed belt and suspenders type validations so I'm keeping this here, commented out! My HTML inputs have required fields on them.
+    else {
+      greeting.innerText = `Dear ${data[0]} ${data[1]}, thank you for reaching out. We will respond to your inquiry as soon as possible.`
+      data.forEach(el => {
+        const entry = document.createElement("li")
+        entry.innerText = el
+        userQuestion.appendChild(entry)
+      })
+      form.reset()
+      userQuestionDisplayed = !userQuestionDisplayed
+      toggleFormBtn.checked = !toggleFormBtn.checked
+      toggleFunc()
+    }
+  })
+}
 
 
 const workInProgress = () => {
